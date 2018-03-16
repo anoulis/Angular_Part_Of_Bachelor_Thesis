c6b0bdda23b5c057ca7875e9167b78aacad83610
anoulis <arisnoul@csd.auth.gr>
2018-03-17 01:39:40 +0200

    more updates for heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Install express server
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -7,3 +7,18 @@ app.use(express.static(__dirname + 'dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+*/
+const express = require('express');
+const http = require('http')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
